0af625a89037c2c499dce08e487f24e5672e6437
GitHubAccount69 <officialmichaelaldrich@gmail.com>
2015-04-24 10:13:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 print "Let's print stuff!"
-print "Say your name, and something cool about yourself!"
+print "My name is Michael F. Aldrich, and my crawlspace is full!"
 
 # each person should put a print statement for themselves
 # for example: print "Hi, I'm John, and I like pie."
