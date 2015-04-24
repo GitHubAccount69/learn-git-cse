44c79afd683370cf86c7814c9185a48857cadcb8
GitHubAccount69 <12aldmic878@BA-1432-DT-01.bville.org>
2015-04-24 10:31:25 -0400

    added instructions

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 print "Let's print stuff!"
 print "Say your name, and something cool about yourself!"
-
+print "My name is Michael F. aldrich, and my crawlspace is full!"
 # each person should put a print statement for themselves
 # for example: print "Hi, I'm John, and I like pie."
 
